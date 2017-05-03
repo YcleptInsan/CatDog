6e9041f34b630932ee4d581818905e7ff1a8f47b
John Dowd <john.dowd@inspectall.com>
2017-05-02 21:02:08 -0700

    inital set up aand logic for backend

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !=== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//USER INTERFACE   FRONT - END//
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
