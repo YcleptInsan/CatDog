864884bca76c4cf8e59170d340d25d3e21b3be70
John Dowd <john.dowd@inspectall.com>
2017-05-02 21:28:00 -0700

    add user logic to the program

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
+
+//BUSINESS LOGIC  (BACKEND)
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !=== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
 };
 
-//USER INTERFACE   FRONT - END//
+//USER INTERFACE   (FRONTEND)
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {          //same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
