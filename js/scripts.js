8872689d675f356d68ceff00ff60550f295de30c
John Dowd and nick wise <nick@>
2017-05-04 16:37:18 -0700

    add set up files and chaged logic for the array

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,45 @@
 
 var catDog = function(number) {
   var result = [];
-  var count = [];
-  if (count  > 0) {
-    for (var i = 1; i <= count; i++) {
+  // if (number > 0) {
+  //   if ((number % 3 === 0) && (number % 5 === 0)) {
+  //     result += ("cat-dog");
+  //   } else if (number % 3 === 0) {
+  //     result += ("dog");
+  //   } else if (number % 5 === 0) {
+  //     result += ("cat");
+  //   } else {
+  //     result += (number);
+  //   }
+
+    for (var i = 0; i <= number; i++) {
       if ((i % 3 === 0) && (i % 5 === 0)) {
-        result.push("cat-dog")
-        console.log(result)
+        result.push("cat-dog");
       } else if (i % 3 === 0) {
         result.push("dog");
       } else if (i % 5 === 0) {
-        result.push("cat-dog");
+        result.push("cat");
       } else {
         result.push(i);
       }
+      console.log(result);
+      var endVal = result.splice(-1, 1);
     }
-  } else if (count === 0) {
-    result.push("Zero does not work!!");
-  } else  if (count < 0){
-    result.push("Don't be negative!");
+    return endVal;
   }
-}
+  // } else if (number === 0) {
+  //   result.push("Zero does not work!!");
+  // } else  if (number < 0){
+  //   result.push("Don't be negative!");
+  // }
+// }
 
 
 $(document).ready(function() {
   $("form#cat-dog").submit(function(event) {
     event.preventDefault();
-    var number =parseInt($("input#number").val());
-    (number);
-
-    for (var i = 0; i <= result.length - 1; i++) {
-
-    }
+    var userNumber = parseInt($("input#number").val());
+    $("#result").append("<p>" + catDog(userNumber) + "</p>");
 
   });
 });
