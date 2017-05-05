5bc83f3c7ae76082ed50cef63890eec034a74057
John Dowd and nick wise <nick@>
2017-05-04 17:34:05 -0700

    add remove previous element

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 
 var catDog = function(number) {
   var result = [];
+  var symbols = ["0"]
   // if (number > 0) {
   //   if ((number % 3 === 0) && (number % 5 === 0)) {
   //     result += ("cat-dog");
@@ -15,21 +16,24 @@ var catDog = function(number) {
   //   }
 
     for (var i = 0; i <= number; i++) {
-      if ((i % 3 === 0) && (i % 5 === 0)) {
-        result.push("cat-dog");
-      } else if (i % 3 === 0) {
-        result.push("dog");
-      } else if (i % 5 === 0) {
-        result.push("cat");
-      } else {
-        result.push(i);
+
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+          result.push("cat-dog");
+        } else if (i % 3 === 0) {
+          result.push("dog");
+        } else if (i % 5 === 0) {
+          result.push("cat");
+        } else {
+          alert("")
+        }
+
+        var endVal = result.splice(-1, 1);
+
+
       }
-      console.log(result);
-      var endVal = result.splice(-1, 1);
-    }
-    return endVal;
+      return endVal;
   }
-  // } else if (number === 0) {
+  // } else if (number === !0) {
   //   result.push("Zero does not work!!");
   // } else  if (number < 0){
   //   result.push("Don't be negative!");
@@ -42,6 +46,6 @@ $(document).ready(function() {
     event.preventDefault();
     var userNumber = parseInt($("input#number").val());
     $("#result").append("<p>" + catDog(userNumber) + "</p>");
-
+     $("p").prev().remove();
   });
 });
